62349413a3479d9ee40401f662458b5dddae0e5a
Alexis-BX <alexisroger99@gmail.com>
2023-01-19 17:00:40 -0500

    squelette du code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Devoir 1
+Le PDF avec les consignes est sur Studium.
+
+Si vous faites un fork pour travailler sur le code (recommandé) mettez le en PRIVÉ!
---
+++ src/Palindrome.java
@@ -0,0 +1,26 @@
+public class Palindrome {
+    public static boolean checker(String input) {
+        // TODO
+        return true;
+    }
+
+    public static boolean checker_recursive(String input) {
+        // TODO
+        return true;
+    }
+
+    public static String reverse(String input) {
+        // TODO
+        return input;
+    }
+
+    public static String transforme_palindrome(String input) {
+        // TODO
+        return input;
+    }
+
+    public static String transforme_palindrome_compacte(String input) {
+        // TODO
+        return input;
+    }
+}   
