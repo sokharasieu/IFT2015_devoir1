ee1c1231f2b783b5bb1168fe7fc87d856200ffe9
sokharasieu <sokhara.sieu@gmail.com>
2023-01-31 15:24:05 -0500

    q2

--- src/App.java
+++ src/App.java
@@ -1,39 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // 2.1 checker
-        if (Palindrome.checker("radar") != true){
+        if (Palindrome.checker("radar") != true) {
             System.out.println("2.1 'checker' function wrong output with input 'radar'");
             return;
         }
-        if (Palindrome.checker("rede") != false){
+        if (Palindrome.checker("rede") != false) {
             System.out.println("2.1 'checker' function wrong output with input 'rede'");
             return;
         }
 
         // 2.2 checker_recursive
-        if (Palindrome.checker_recursive("radar") != true){
+        if (Palindrome.checker_recursive("radar") != true) {
             System.out.println("2.2 'checker_recursive' function wrong output with input 'radar'");
             return;
         }
-        if (Palindrome.checker_recursive("rede") != false){
+        if (Palindrome.checker_recursive("rede") != false) {
             System.out.println("2.2 'checker_recursive' function wrong output with input 'rede'");
             return;
         }
 
         // 2.3 reverse
-        if (!Palindrome.reverse("qwer").equals("rewq")){
+        if (!Palindrome.reverse("qwer").equals("rewq")) {
             System.out.println("2.3 'reverse' function wrong output with input 'qwer'");
             return;
         }
 
         // 2.4 transforme_palindrome
-        if (!Palindrome.transforme_palindrome("qwere").equals("qwereerewq")){
+        if (!Palindrome.transforme_palindrome("qwere").equals("qwereerewq")) {
             System.out.println("2.4 'transforme_palindrome' function wrong output with input 'qwere'");
             return;
         }
 
         // 2.5 transforme_palindrome_compacte
-        if (!Palindrome.transforme_palindrome_compacte("qwere").equals("qwerewq")){
+        if (!Palindrome.transforme_palindrome_compacte("qwere").equals("qwerewq")) {
             System.out.println("2.5 'transforme_palindrome_compacte' function wrong output with input 'qwere'");
             return;
         }
@@ -53,7 +53,7 @@ public class App {
         queue_double.push(true, 3);
         queue_double.push(false, 2);
         // queue_double.print()
-        // affiche: 
+        // affiche:
         // pile one: (3)
         // pile two: (2)
 
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,26 +1,70 @@
+
 public class Palindrome {
+
+    public static String toSubstring(int start, int end, String str) {
+        String result = "";
+        for (int i = start; i < end; i++) {
+            result += str.charAt(i);
+        }
+        return result;
+    }
+
     public static boolean checker(String input) {
-        // TODO
+        // check length of string
+        // start from start and from end
+        // if they are not the same return false
+        for (int i = 0, j = input.length() - 1; i < j; i++, j--) {
+            if (input.charAt(i) != input.charAt(j)) {
+                return false;
+            }
+        }
         return true;
     }
 
+    // check if a string is a palindrome trough a recursive method
     public static boolean checker_recursive(String input) {
-        // TODO
-        return true;
+
+        if (input.length() == 0 || input.length() == 1) {
+            return true;
+        }
+        if (input.charAt(0) == input.charAt(input.length() - 1)) {
+            String resultString = "";
+            for (int i = 1; i < input.length() - 1; i++) {
+                resultString += input.charAt(i);
+            }
+
+            return checker_recursive(resultString);
+        }
+
+        return false;
     }
 
     public static String reverse(String input) {
-        // TODO
-        return input;
+        String result = "";
+        char ch;
+        for (int i = 0; i < input.length(); i++) {
+            ch = input.charAt(i);
+            result = ch + result;
+        }
+        return result;
     }
 
     public static String transforme_palindrome(String input) {
-        // TODO
-        return input;
+        return input + reverse(input);
     }
 
     public static String transforme_palindrome_compacte(String input) {
-        // TODO
+        if (checker(input)) {
+            return input;
+        } else {
+            for (int i = 1; i < input.length(); i++) {
+                if (checker(toSubstring(i, input.length(), input))) {
+                    return input + reverse(toSubstring(0, i, input));
+                }
+            }
+
+        }
+
         return input;
     }
-}   
+}
